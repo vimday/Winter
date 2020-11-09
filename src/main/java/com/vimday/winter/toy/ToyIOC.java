ee9edd5fb8c8c2e5a803f271007af4e60721ffba
vimday <lwfplus@gmail.com>
2020-11-09 12:42:00 +0800

    init project

---
+++ src/main/java/com/vimday/winter/toy/ToyIOC.java
@@ -0,0 +1,19 @@
+/*
+  Copyright (C) 2020 Daydream Limited, a vimday company. No rights reserved.
+ */
+
+package com.vimday.winter.toy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author vimdayli
+ * @version 1.0
+ * @description: TODO
+ * @date 11/8/2020 10:00 PM
+ */
+public class ToyIOC {
+    private final Map<String,Object> beanMap = new HashMap<String, Object>();
+
+}
